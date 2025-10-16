9b9a557856205b3042be429186eb0e5916b5ce52
AAbhinVV <abhinnnn7890@gmail.com>
2025-10-16 14:06:25 +0530

    fix: correct email validation in register schema

--- server/config/zod.js
+++ server/config/zod.js
@@ -2,6 +2,7 @@ import {z} from 'zod';
 
 export const registerSchema = z.object({
     username: z.string().min(3, "Name must be at least 3 characters long"),
-    email: z.string().email("invalid email format"),
-    password: z.string().min(8, "Password must be at least 8 characters long")
+    email: z.email()("invalid email format"),
+    password: z.string().min(8, "Password must be at least 8 characters long"),
+    phone_number: z.string().min(7, "Phone must be at least 7 digits")
 })
