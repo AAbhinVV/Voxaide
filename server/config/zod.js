c8a5e63763dec872b1845568a9b6f685cc06052f
AAbhinVV <abhinnnn7890@gmail.com>
2025-10-22 16:45:53 +0530

    hm

--- server/config/zod.js
+++ server/config/zod.js
@@ -1,8 +1,16 @@
 import {z} from 'zod';
 
-export const registerSchema = z.object({
+const registerSchema = z.object({
     username: z.string().min(3, "Name must be at least 3 characters long"),
     email: z.email()("invalid email format"),
     password: z.string().min(8, "Password must be at least 8 characters long"),
     phone_number: z.string().min(7, "Phone must be at least 7 digits")
 })
+
+const loginSchema = z.object({
+    username: z.string().min(3, "Name must be at least 3 characters long"),
+    email: z.email()("invalid email format"),
+    password: z.string().min(8, "Password must be at least 8 characters long"),
+})
+
+export {registerSchema, loginSchema}
