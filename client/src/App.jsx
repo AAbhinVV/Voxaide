5f4408e64024c3ebcf47fd7ce0ea2abc8ad4a3ab
AAbhinVV <abhinnnn7890@gmail.com>
2025-12-06 20:38:18 +0530

    loader updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,33 +10,39 @@ import Navbar from './components/navbar'
 import LoadingScreen from './components/LoadingScreen'
 import LoadScreen from './components/LoadScreen'
 import { LoaderOne } from './components/ui/loader'
+import Loader from './components/testloader.jsx'
+
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true)
-  const [isFadingOut, setIsFadingOut] = useState(false)
+  // const [isLoading, setIsLoading] = useState(true)
+  // const [isFadingOut, setIsFadingOut] = useState(false)
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsFadingOut(true) // start fade out animation
-    }, 4000)// show loading screen for 4 seconds
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     setIsFadingOut(true) // start fade out animation
+  //   }, 4000)// show loading screen for 4 seconds
   
-    return () => clearTimeout(timer);
-  }, []); // duration of fade-out animation
+  //   return () => clearTimeout(timer);
+  // }, []); // duration of fade-out animation
 
 
 
-  //wait for fadeout duration to complete before removing the ocmponenet
-  const handleAnimationEnd = () => {
-    if (isFadingOut) {
-      setIsLoading(false)
-    }
-  };
+  // //wait for fadeout duration to complete before removing the ocmponenet
+  // const handleAnimationEnd = () => {
+  //   if (isFadingOut) {
+  //     setIsLoading(false)
+  //   }
+  // };
 
-  if(isLoading){
-    return <LoaderOne className={`${isFadingOut ? 'opacity-0' : 'opacity-100'} transition-opacity duration-500`} 
-    onAnimationEnd={handleAnimationEnd} />
+  // if(isLoading){
+  //   return <LoaderOne className={isFadingOut ? "fade-out" : ""} onAnimationEnd={handleAnimationEnd} />
+  // }
+
+  const [showLoader, setShowLoader] = useState(true);
+
+  if (showLoader) {
+    return <Loader onComplete={() => setShowLoader(false)} />;
   }
-
   return (
     <>
       
