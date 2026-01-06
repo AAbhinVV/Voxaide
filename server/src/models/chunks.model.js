3c049422bbeae80c3aecb51127d3e73b54c48e5c
AAbhinVV <abhinnnn7890@gmail.com>
2026-01-06 21:27:39 +0530

    chuking, embedding, vector storage implemented

--- server/src/models/chunks.model.js
+++ server/src/models/chunks.model.js
@@ -2,16 +2,17 @@ import mongoose from "mongoose";
 
 const ChunkSchema = new mongoose.Schema({
     userId: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, index: true},
-    transcriptionId: { type: mongoose.Schema.Types.ObjectId, ref: 'Transcription', required: true, index: true },
+    transcriptionId: { type: mongoose.Schema.Types.ObjectId, ref: 'Transcription', required: true, index: true},
     chunkIndex: { type: Number, required: true },
     text: { type: String, required: true },
-    embeddingId: { type: String, required: true, index: true },
+    embeddingId: { type: String, index: true },
 }, { 
     timestamps: true,
     collection: 'chunks'
  });
 
 ChunkSchema.index({ userId: 1, transcriptionId: 1 });
+ChunkSchema.index({transcriptionId: 1, chunkIndex: 1}, {unique: true});
 
 
-export default mongoose.model('Chunk', ChunkSchema);
+export default mongoose.model('TranscriptionChunk', ChunkSchema);
--- server/src/services/embedding.service.js
+++ server/src/services/embedding.service.js
@@ -1,14 +1,89 @@
-const embeddingService = async (transcriptionText) => {
-    //chunking the text
-    const chunks = [];
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { encoding_for_model } from "tiktoken";
+import { PineconeStore } from "@langchain/pinecone";
+import { Pinecone as PineconeClient } from "@pinecone-database/pinecone";
+import constants from "../config/constant.js";
+import TrancriptionChunk from "../models/chunks.model.js";
+
+
+
+
+/**
+ * @param {ObjectId} mongoDocId - MongoDB document _id
+ * @param {string} text - transcription or document text
+ * @param {Object} extraMetadata - optional metadata
+ */
+const generateChunksAndEmbeddings = async ({
+    transcriptionId,
+    userId,
+    transcriptionText
+}) => {
+    //********************************************chunking the text************************************************
+    const encoder = encoding_for_model("text-embedding-3-large");
+
+    const splitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 1000,
+        chunkOverlap: 200,
+        separators: ["\n\n", "\n", " ", ""],
+        lengthFunction: (text) => {
+            const tokens = encoder.encode(text).length;
+            return tokens;
+        }
+    })
+
+    const chunks = await splitter.splitText(transcriptionText);
+    encoder.free();
+
+    if(!chunks || chunks.length ===0){
+        throw new Error("No chunks generated from the transcription text");
+    }
+
+    const chunkDocs = chunks.map((text, index) => ({
+        userId,
+        transcriptionId,
+        chunkIndex: index,
+        text
+    }))
+
+    const savedChunks = await TranscriptionChunk.insertMany(chunkDocs);
+
+
+    //**********************************creating embeddings for each chunk**************************************
+    const embeddings = new OpenAIEmbeddings({
+        modelName: "text-embedding-3-large"
+    });
+
     
 
-    //creating embeddings for each chunk
+
+    //********************************************storing in pinecone************************************************
+    const pinecone= new PineconeClient({
+        apiKey: constants.pinecone_api_key,
+    })
+
+    const pineconeIndex = pinecone.Index("voxaide");
+
+    const vectorStore = new PineconeStore(embeddings, {
+        pineconeIndex,
+        maxConcurrency: 10,
+    })
 
 
-    //storing in pinecone
+     const metadatas = chunks.map((_, index) => ({
+    mongoDocId: mongoDocId.toString(),
+    chunkIndex: index,
+    userId: userId?.toString(),
+    source,
+    }));
 
+    await vectorStore.addTexts(chunks, metadatas);
 
-    //storing metadata in mongo
+    return {
+        mongoDocId,
+        totalChunks: chunks.length
+    }
+
+    
 
 }
